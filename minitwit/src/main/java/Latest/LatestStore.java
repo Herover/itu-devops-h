9c8b1600164473087517eaedd8ab1b2040dfe2e7
Herover <hi+git@leonora.app>
2022-04-29 14:00:00 +0200

    Use autoclosables for sql

--- minitwit/src/main/java/Latest/LatestStore.java
+++ minitwit/src/main/java/Latest/LatestStore.java
@@ -6,42 +6,38 @@ import java.sql.SQLException;
 
 public class LatestStore {
     public int getLatest() throws SQLException {
-        var connection = new SqlDatabase().getConnection();
+        try (var connection = new SqlDatabase().getConnection();
+             var statement = connection.createStatement()) {
+            var result = statement.executeQuery("SELECT * FROM counters WHERE kind = 'latest' LIMIT 1");
+            int value;
 
-        var statement = connection.createStatement();
-        var result = statement.executeQuery("SELECT * FROM counters WHERE kind = 'latest' LIMIT 1");
-        int value;
+            if (result.next()) {
+                value = result.getInt("value");
+            } else {
+                // If there's no result, silently return -1
+                value = -1;
+            }
 
-        if (result.next()) {
-            value = result.getInt("value");
-        } else {
-            // If there's no result, silently return -1
-            value = -1;
+            statement.close();
+
+            return value;
         }
-
-        statement.close();
-        connection.close();
-
-        return value;
     }
 
     public void updateLatest(int latest) throws SQLException {
-        var connection = new SqlDatabase().getConnection();
+        try (var connection = new SqlDatabase().getConnection();
+             // Insert a row with the latest value unless there's already a counter with the kind "latest", in that case
+             // update it.
+             var statement = connection.prepareStatement("INSERT INTO counters (kind, value) \n" +
+                     "VALUES ('latest', ?) \n" +
+                     "ON CONFLICT (kind) DO UPDATE \n" +
+                     "  SET kind = 'latest', \n" +
+                     "      value = ?");
+        ) {
+            statement.setInt(1, latest);
+            statement.setInt(2, latest);
 
-        // Insert a row with the latest value unless there's already a counter with the kind "latest", in that case
-        // update it.
-        var statement = connection.prepareStatement("INSERT INTO counters (kind, value) \n" +
-                "VALUES ('latest', ?) \n" +
-                "ON CONFLICT (kind) DO UPDATE \n" +
-                "  SET kind = 'latest', \n" +
-                "      value = ?");
-
-        statement.setInt(1, latest);
-        statement.setInt(2, latest);
-
-        statement.execute();
-
-        statement.close();
-        connection.close();
+            statement.execute();
+        }
     }
 }
